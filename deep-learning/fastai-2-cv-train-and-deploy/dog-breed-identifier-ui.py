d8e08880ba856336f007170214e1500279799ba1
Dima Timofeev <1127655+CuriousDima@users.noreply.github.com>
2024-06-30 21:03:46 -0700

    get back to huggingfacce-hub

--- deep-learning/fastai-2-cv-train-and-deploy/dog-breed-identifier-ui.py
+++ deep-learning/fastai-2-cv-train-and-deploy/dog-breed-identifier-ui.py
@@ -1,14 +1,10 @@
 from copy import deepcopy
 
 from fastai.vision.all import *
-
-# from huggingface_hub import from_pretrained_fastai
+from huggingface_hub import from_pretrained_fastai
 from PIL import Image
-
-# from streamlit_js_eval import get_page_location
 import pandas as pd
 import streamlit as st
-import pathlib
 
 
 _LABELS = (
@@ -141,9 +137,7 @@ def get_breed(path):
 
 @st.cache_resource
 def get_predictor():
-    # return from_pretrained_fastai("TheDima/resnet50-dog-breed-identification")
-    path = pathlib.Path("dog-breed-identification-export_error-rate-0_118.pkl")
-    return load_learner(path)
+    return from_pretrained_fastai("TheDima/resnet50-dog-breed-identification")
 
 
 def predict(image):
@@ -161,19 +155,16 @@ def print_probabilities(probs, labels, top_n=10):
 
 st.title("Dog Breed Recognition")
 
-# url = get_page_location()
-# image_prefix = url["protocol"] + "//" + url["host"]
-
-# st.write("Some doggies for your inspiration")
-# d1, d2, d3, d4 = st.columns(4)
-# with d1:
-#     st.image("static/0a54ce47525781f2caa66f65291dddf8.jpg")
-# with d2:
-#     st.image("static/0a59d3205cff15e31ee30213b9988e7e.jpg")
-# with d3:
-#     st.image("static/0ab808aa3571846e50ed74a204662c52.jpg")
-# with d4:
-#     st.image("static/0c0b3758c1b177b2a2961c1483159898.jpg")
+st.write("Some doggies for your inspiration")
+d1, d2, d3, d4 = st.columns(4)
+with d1:
+    st.image("static/0a54ce47525781f2caa66f65291dddf8.jpg")
+with d2:
+    st.image("static/0a59d3205cff15e31ee30213b9988e7e.jpg")
+with d3:
+    st.image("static/0ab808aa3571846e50ed74a204662c52.jpg")
+with d4:
+    st.image("static/0c0b3758c1b177b2a2961c1483159898.jpg")
 
 
 uploaded_file = st.file_uploader("Upload a doggy...", type=["jpg", "jpeg"])
